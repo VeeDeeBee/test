5a0c0ae880353b991b73082d2162549fa2d51ef3
VeeDeeBee <rubenvandenberghe@gmail.com>
2021-06-27 20:55:46 +0200

    new screen layout

---
+++ include/constants.h
@@ -0,0 +1,2 @@
+const char* ssid = "telenet-8D77614";
+const char* password = "fp6tVdfertsR";
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Adafruit_SSD1327.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <constants.h>
 
-// Used for software SPI
-#define OLED_CLK 13
-#define OLED_MOSI 11
-
-// Used for software or hardware SPI
-#define OLED_CS 10
-#define OLED_DC 8
+ESP8266WebServer server(80);
 
 // Used for I2C or SPI
 #define OLED_RESET -1
+
 
 // software SPI
 //Adafruit_SSD1305 display(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
@@ -19,193 +17,45 @@
 // I2C
 Adafruit_SSD1327 display(128, 128, &Wire, OLED_RESET, 1000000);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextWrap(false);
-  display.setTextColor(SSD1327_WHITE);
-  display.setCursor(0,0);
-
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }    
-  display.display();
+String IpAddress2String(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + String(".") +\
+  String(ipAddress[1]) + String(".") +\
+  String(ipAddress[2]) + String(".") +\
+  String(ipAddress[3])  ; 
 }
 
-void testdrawcircle(void) {
-  for (uint8_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, i % 15 + 1);
-    display.display();
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (uint8_t i=0; i<display.height()/2; i+=3) {
-    // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2,  i % 15 + 1);
-    display.display();
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (uint16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i,  i % 15 + 1);
-    display.display();
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = SSD1327_WHITE;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i,  i % 15 + 1);
-    display.display();
-  }
-}
-
-void testdrawroundrect(void) {
-  for (uint8_t i=0; i<display.height()/3-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4,  i % 15 + 1);
-    display.display();
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = SSD1327_WHITE;
-  for (uint8_t i=0; i<display.height()/3-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4,  i % 15 + 1);
-    display.display();
-  }
-}
-   
-void testdrawrect(void) {
-  for (uint8_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, i % 15 + 1);
-    display.display();
-  }
-}
-
-void testdrawline() {  
-  for (uint8_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, SSD1327_WHITE);
-    display.display();
-  }
-  for (uint8_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, SSD1327_WHITE);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (uint8_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, SSD1327_WHITE);
-    display.display();
-  }
-  for (int8_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, SSD1327_WHITE);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int8_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, SSD1327_WHITE);
-    display.display();
-  }
-  for (int8_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, SSD1327_WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (uint8_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, SSD1327_WHITE);
-    display.display();
-  }
-  for (uint8_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, SSD1327_WHITE); 
-    display.display();
-  }
-  delay(250);
-}
-
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
-  randomSeed(666);     // whatever seed
- 
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-    
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, f+1);
-    }
-    display.display();
-    delay(200);
-    // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, SSD1327_BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-      	icons[f][XPOS] = random(display.width());
-      	icons[f][YPOS] = 0;
-      	icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-   }
-}
 
 void setup()   {                
   Serial.begin(9600);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  int i = 0;
+
+  while ( i < 60 && WiFi.status() != WL_CONNECTED ) 
+  {
+    Serial.print(".");
+    delay(500);
+    i++;
+  }
+
+  Serial.println();
+
+  if (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.println("[WARNING] Connection timed out, offline mode enabled.");
+  } 
+  else
+  {
+    Serial.print("Connected! IP address: ");
+    String ip = IpAddress2String(WiFi.localIP());
+    Serial.println(ip);
+  }
+
   //while (! Serial) delay(100);
   Serial.println("SSD1327 OLED test");
   
@@ -216,107 +66,84 @@ void setup()   {
   display.clearDisplay();
   display.display();
 
-  display.setTextSize(10);
+  // display.setTextSize(10);
+  // display.setTextWrap(false);
+  // display.setTextColor(0xF);
+  // display.setCursor(0,0);
+  // display.write("Ruben");
+  // display.display();
+
+
+  char * header;
+  char * screen1header;
+  char * screen1value;
+  char * screen2header;
+  char * screen2value;
+  char * screen3header;
+  char * screen3value;
+  char * screen4header;
+  char * screen4value;
+  char * footer;
+
+  header = "Solar";
+  screen1header = "Current";
+  screen1value = "123";
+  screen2header = "Max today";
+  screen2value = "456";
+  screen3header = "This month";
+  screen3value = "789";
+  screen4header = "This year";
+  screen4value = "abc";
+  footer = "Last update: ";
+
+  display.setTextSize(2);
   display.setTextWrap(false);
   display.setTextColor(0xF);
-  display.setCursor(0,0);
-  display.write("Ruben");
+  display.setCursor(0,10);
+  display.write(header);
+
+  display.drawLine(0, 30, display.width()-1, 30 , 0x5);
+  display.drawLine(0, display.height()-20, display.width()-1 , display.height()-20 , 0x5);
+  display.drawLine(display.width()/2, 30, display.width()/2, display.height()-20 , 0x2);
+  display.drawLine(0, display.height()/2+5, display.width()-1, display.height()/2+5 , 0x5);
+
+  display.setTextSize(1);
+  display.setTextColor(0x5);
+
+  display.setCursor(0,32);
+  display.write(screen1header);
+
+  display.setCursor(display.width()/2+2,32);
+  display.write(screen2header);
+  
+  display.setCursor(0,display.height()/2+7);
+  display.write(screen3header);
+
+  display.setCursor(display.width()/2+2,display.height()/2+7);
+  display.write(screen4header);
+
+  display.setCursor(0,display.height()-18);
+  display.write(footer);
+  
+  display.setTextSize(2);
+  display.setTextColor(0xF);
+
+  display.setCursor(0,47);
+  display.write(screen1value);
+
+  display.setCursor(display.width()/2+2,47);
+  display.write(screen2value);
+  
+  display.setCursor(0,display.height()/2+22);
+  display.write(screen3value);
+
+  display.setCursor(display.width()/2+2,display.height()/2+22);
+  display.write(screen4value);
+
+
   display.display();
 
-  for (int i = 0; i < 20; i++) {
-    display.clearDisplay();
-    display.setCursor(-i*5,0);
-    display.write("Ruben");
-    display.display();
-    delay(100);
-  } 
-
-  // // draw many lines
-  // testdrawline();
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // // draw rectangles
-  // testdrawrect();
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // // draw multiple rectangles
-  // testfillrect();
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // // draw mulitple circles
-  // testdrawcircle();
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // // draw a SSD1327_WHITE circle, 10 pixel radius
-  // display.fillCircle(display.width() / 2, display.height() / 2, 10, SSD1327_WHITE);
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // testdrawroundrect();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // testfillroundrect();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // testdrawtriangle();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // testfilltriangle();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // // draw the first ~12 characters in the font
-  // testdrawchar();
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-
-  // for (uint8_t rot = 0; rot < 4; rot++)
-  // {
-  //   display.setRotation(rot);
-  //   display.clearDisplay();
-  //   // text display tests
-  //   display.setTextSize(1);
-  //   display.setTextColor(SSD1327_WHITE);
-  //   display.setCursor(0, 0);
-  //   display.println("Hello, world!");
-  //   display.setTextColor(SSD1327_BLACK, SSD1327_WHITE); // 'inverted' text
-  //   display.println(3.141592);
-  //   display.setTextSize(2);
-  //   display.setTextColor(SSD1327_WHITE);
-  //   display.print("0x");
-  //   display.println(0xDEADBEEF, HEX);
-  //   display.display();
-  //   delay(1000);
-  // }
-
-  // display.setRotation(0);
-
-  // // miniature bitmap display
-  // display.clearDisplay();
-  // display.drawBitmap(30, 16, logo16_glcd_bmp, 16, 16, 1);
-  // display.display();
-
-  // // invert the display
-  // display.invertDisplay(true);
-  // delay(1000);
-  // display.invertDisplay(false);
-  // delay(1000);
-
-  // // draw a bitmap icon and 'animate' movement
-  // testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+  
 }
 
 
